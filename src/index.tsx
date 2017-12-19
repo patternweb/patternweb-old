b5423eb46ddc5d87f10707f6c7446d63dc1bcaab
John Rees <johnrees@users.noreply.github.com>
2017-12-19 16:20:05 +0000

    rendering code and graph with react

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("graph"));
+import Code from "./views/code";
+import Graph from "./views/graph";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="ui">
+        <Code />
+        <Graph />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("container"));
---
+++ src/views/code.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function Code() {
+  return <div id="code" />;
+}
