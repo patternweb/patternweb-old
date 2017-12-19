a5e3e946560b2eacd8f3b0e660cc2e7b2be7e046
John Rees <johnrees@users.noreply.github.com>
2017-12-19 16:06:55 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    app: "./src/index.tsx"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  output: {
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "./dist/js/")
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./public"),
+    hot: false,
+    inline: false,
+    host: "0.0.0.0"
+  },
+  node: {
+    process: false,
+    Buffer: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["env"] }
+          },
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
